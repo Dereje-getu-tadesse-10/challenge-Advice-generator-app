c5f706bb6c0086c0fe36c0a16fa4485054452bd3
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2022-05-17 14:19:33 +0200

    upadate button for get advice

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-const track = document.querySelector('#track');
-const advice = document.querySelector('#advice');
-const getAdvice = document.querySelector('.greenCircle');
+const track = document.querySelector("#track");
+const advice = document.querySelector("#advice");
+const getAdvice = document.querySelector(".greenCircle");
 const url = `https://api.adviceslip.com/advice`;
+let countAdvice = 0;
 
-// call api
-const apiCall = () =>{
-
-    fetch(url)
-    .then((responses)=>{return responses.json()})
-    .then((data)=>{
-        console.log(data);
-        advice.textContent = `“${data.slip.advice}”`;
-        track.textContent = `ADVICE #${data.slip.id}`;
+// function with parameter
+const apiCall = (link) => {
+  fetch(link)
+    .then((responses) => {
+      return responses.json();
+    })
+    .then((data) => {
+      // add advice into html element
+      advice.textContent = `“${data.slip.advice}”`;
+      track.textContent = `ADVICE #${data.slip.id}`;
     });
-}
+};
 
-// get another advice with 
-getAdvice.addEventListener("click", ()=> {
-   location.reload();
+// button for get another advice on click
+getAdvice.addEventListener("click", () => {
+  apiCall(url);
+  countAdvice += 1;
+  console.log(
+    `%cYou generated ${countAdvice} advice`,
+    "background-color:hsl(217, 19%, 38%); padding:10px; color:white; border-radius:.5em"
+  );
 });
 
-apiCall();
+// call function for get advice
+apiCall(url);
